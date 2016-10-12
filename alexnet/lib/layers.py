af096cfa583c0800280a0f3c688c2f33198261ab
cohenmao <cohenmao@gmail.com>
2016-10-12 22:27:33 +0300

    beginning of modifications towards wavenet

--- alexnet/lib/layers.py
+++ alexnet/lib/layers.py
@@ -40,6 +40,25 @@ class Weight(object):
         self.val.set_value(self.np_values)
 
 
+class FilterBankWeight(object):
+
+    def __init__(self, w_val):
+        super(Weight, self).__init__()
+        self.np_values = np.cast[theano.config.floatX](
+            np.array(w_val, dtype=theano.config.floatX))
+
+        self.val = theano.shared(value=self.np_values)
+
+    def save_weight(self, dir, name):
+        print 'weight saved: ' + name
+        np.save(dir + name + '.npy', self.val.get_value())
+
+    def load_weight(self, dir, name):
+        print 'weight loaded: ' + name
+        self.np_values = np.load(dir + name + '.npy')
+        self.val.set_value(self.np_values)
+
+
 class DataLayer(object):
 
     def __init__(self, input, image_shape, cropsize, rand, mirror, flag_rand):
@@ -69,6 +88,141 @@ class DataLayer(object):
             :, crop_xs:crop_xs + cropsize, crop_ys:crop_ys + cropsize, :]
 
         print "data layer with shape_in: " + str(image_shape)
+
+
+def scatter_progression_filter(filter_bank, desired_filter_shape, idx_to_progress, idx_to_apply):
+    """ receives a filter bank and creates a filter designed to imitate one edge in the scatter tree
+
+    :param filter_bank: 3D tensor of pre-designed 2D filters (filter_x_axis, filter_y_axis, filter_idx)
+    :param desired_filter_shape: dimensions of filter in order for it to match desired response from previous
+            layer (filter_x_axis, filter_y_axis, response_idx)
+    :param idx_to_progress: the index in the response from the previous layer to progress
+    :param idx_to_apply: the index of the filter in <filter_bank> designated for convolution by the filter
+    :return: filter_out: a filter which convoloves a specific filter from the filter bank  with a specific
+            feature map from the previous layer
+    """
+
+    # space dimensions must be equal between filter bank and desired filter
+    assert all([filter_bank.shape[ii] == desired_filter_shape[ii] for ii in range(2)])
+    # filter is zeros everywhere except in the specific response we whish to progress. In that idx,
+    # insert the filter we wish to apply on the response:
+    filter_out = np.zeros(desired_filter_shape)
+    filter_out[:, :, idx_to_apply] = filter_bank[:, :, idx_to_progress]
+
+    return filter_out
+
+
+def progress_scatter_layer(input, input_scales, filter_bank, filter_scales, weights, padsize, convstride):
+    """ convolve an input with the filter bank following the scattering architecture
+
+    :param input: data from the previous layer
+    :param input_scales: the scale of the last wavelet filter in the path of the corresponding response
+    :param filter_bank: filters to use on <input>
+    :param filter_scales: the scale of the last wavelet filter
+    :param weights: weight given to each filter (learned from the data)
+    :param padsize: num pixels to pad for convolution
+    :param convstride: stride of convolution
+    :return: output: data after progression
+             output_scales: scale of the wavelet filter applied on the corresponding output response
+    """
+
+    batchsize = input.shape[0]
+    n_input_responses = input.shape[1]
+    n_filters = filter_bank.shape[0]
+    output_size = (input.shape[2] - filter_bank.shape[2] + 2*padsize)/convstride
+    n_output_responses = calc_n_responses(input_scales, filter_scales)
+    output = np.zeros((batchsize, n_output_responses, output_size, output_size))
+    output_scales = np.zeros(n_output_responses)
+
+    cnt_output_response = 0
+    for idx_response in range(n_input_responses):
+        for idx_filter in range(n_filters):
+            if input_scales[idx_response] < filter_scales[idx_filter]:
+                weight = weights[cnt_output_response]
+                output[:, cnt_output_response, :, :] = dnn.dnn_conv(
+                    img=input[:, idx_response, :, :],
+                    kerns=weight*filter_bank[:, idx_filter, :, :],
+                    subsample=(convstride, convstride),
+                    border_mode=padsize,
+                )
+                output_scales[cnt_output_response] = filter_scales[idx_filter]
+                cnt_output_response += 1
+
+    return output, output_scales
+
+def calc_n_responses(input_scales, filter_scales):
+
+    return len([ii for ii in input_scales for jj in filter_scales if jj > ii])
+
+
+class FilterBankConvPoolLayer(object):
+
+    def __init__(self, input, input_scales, image_shape, filter_bank, filter_scales, filter_shape,
+                 convstride, padsize, group, poolsize, poolstride, bias_init, lrn=False, lib_conv='cudnn',
+                 ):
+
+        self.filter_size = filter_shape
+        self.filter_bank = filter_bank
+        self.input_scales = input_scales
+        self.filter_scales = filter_scales
+        self.convstride = convstride
+        self.padsize = padsize
+        self.poolsize = poolsize
+        self.poolstride = poolstride
+        self.channel = image_shape[0]
+        self.lrn = lrn
+        self.lib_conv = lib_conv
+        assert group == 1
+
+        self.filter_shape = np.asarray(filter_shape)
+        self.image_shape = np.asarray(image_shape)
+
+        if self.lrn:
+            self.lrn_func = CrossChannelNormalization()
+
+        n_responses = calc_n_responses(input_scales, filter_scales)
+        self.W = Weight(n_responses)
+        self.b = Weight(n_responses, bias_init, std=0)
+        self.weighted_filter_bank = FilterBankWeight(
+            np.array([self.filter_bank[:, :, :, f_id]*self.W[f_id] for f_id in range(len(self.W))])
+        )
+
+        input_shuffled = input.dimshuffle(3, 0, 1, 2)  # c01b to bc01
+        # in01out to outin01
+        # print image_shape_shuffled
+        # print filter_shape_shuffled
+        W_shuffled = self.weighted_filter_bank.val.dimshuffle(3, 0, 1, 2)  # c01b to bc01
+        conv_out_complex = progress_scatter_layer(input=input_shuffled,
+                                                  input_scales=self.input_scales,
+                                                  filter_bank=W_shuffled,
+                                                  filter_scales=filter_scales,
+                                                  weights=self.W,
+                                                  padsize=padsize,
+                                                  convstride=convstride
+                                                  )
+        conv_out = T.sqrt(T.sqr(T.real(conv_out_complex)) + T.sqr(T.imag(conv_out_complex))) 
+        conv_out = conv_out + self.b.val.dimshuffle('x', 0, 'x', 'x')
+
+        # ReLu
+        self.output = T.maximum(conv_out, 0)
+
+        # Pooling
+        if self.poolsize != 1:
+            self.output = dnn.dnn_pool(self.output,
+                                       ws=(poolsize, poolsize),
+                                       stride=(poolstride, poolstride))
+
+        self.output = self.output.dimshuffle(1, 2, 3, 0)  # bc01 to c01b
+
+        # LRN
+        if self.lrn:
+            self.output = self.lrn_func(self.output)
+
+        self.params = [self.W.val, self.b.val]
+        self.weight_type = ['W', 'b']
+
+        print "conv ({}) layer with shape_in: {}".format(lib_conv,
+                                                         str(image_shape))
 
 
 class ConvPoolLayer(object):
