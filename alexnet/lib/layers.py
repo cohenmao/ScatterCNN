6ad65d33c5bcecdadcc6127e60b2659b1f530fee
cohenmao <cohenmao@gmail.com>
2016-10-23 09:29:35 +0300

    replace previous code of wavenet layer
    previous code did not handle symbolic variables and new one does

--- alexnet/lib/layers.py
+++ alexnet/lib/layers.py
@@ -8,6 +8,7 @@ from theano.sandbox.cuda.basic_ops import gpu_contiguous
 from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
 from pylearn2.sandbox.cuda_convnet.pool import MaxPool
 from pylearn2.expr.normalize import CrossChannelNormalization
+from wavenet_tools import progress_single_image, greater_than_elementwise
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -90,71 +91,6 @@ class DataLayer(object):
         print "data layer with shape_in: " + str(image_shape)
 
 
-def scatter_progression_filter(filter_bank, desired_filter_shape, idx_to_progress, idx_to_apply):
-    """ receives a filter bank and creates a filter designed to imitate one edge in the scatter tree
-
-    :param filter_bank: 3D tensor of pre-designed 2D filters (filter_x_axis, filter_y_axis, filter_idx)
-    :param desired_filter_shape: dimensions of filter in order for it to match desired response from previous
-            layer (filter_x_axis, filter_y_axis, response_idx)
-    :param idx_to_progress: the index in the response from the previous layer to progress
-    :param idx_to_apply: the index of the filter in <filter_bank> designated for convolution by the filter
-    :return: filter_out: a filter which convoloves a specific filter from the filter bank  with a specific
-            feature map from the previous layer
-    """
-
-    # space dimensions must be equal between filter bank and desired filter
-    assert all([filter_bank.shape[ii] == desired_filter_shape[ii] for ii in range(2)])
-    # filter is zeros everywhere except in the specific response we whish to progress. In that idx,
-    # insert the filter we wish to apply on the response:
-    filter_out = np.zeros(desired_filter_shape)
-    filter_out[:, :, idx_to_apply] = filter_bank[:, :, idx_to_progress]
-
-    return filter_out
-
-
-def progress_scatter_layer(input, input_scales, filter_bank, filter_scales, weights, padsize, convstride):
-    """ convolve an input with the filter bank following the scattering architecture
-
-    :param input: data from the previous layer
-    :param input_scales: the scale of the last wavelet filter in the path of the corresponding response
-    :param filter_bank: filters to use on <input>
-    :param filter_scales: the scale of the last wavelet filter
-    :param weights: weight given to each filter (learned from the data)
-    :param padsize: num pixels to pad for convolution
-    :param convstride: stride of convolution
-    :return: output: data after progression
-             output_scales: scale of the wavelet filter applied on the corresponding output response
-    """
-
-    batchsize = input.shape[0]
-    n_input_responses = input.shape[1]
-    n_filters = filter_bank.shape[0]
-    output_size = (input.shape[2] - filter_bank.shape[2] + 2*padsize)/convstride
-    n_output_responses = calc_n_responses(input_scales, filter_scales)
-    output = np.zeros((batchsize, n_output_responses, output_size, output_size))
-    output_scales = np.zeros(n_output_responses)
-
-    cnt_output_response = 0
-    for idx_response in range(n_input_responses):
-        for idx_filter in range(n_filters):
-            if input_scales[idx_response] < filter_scales[idx_filter]:
-                weight = weights[cnt_output_response]
-                output[:, cnt_output_response, :, :] = dnn.dnn_conv(
-                    img=input[:, idx_response, :, :],
-                    kerns=weight*filter_bank[:, idx_filter, :, :],
-                    subsample=(convstride, convstride),
-                    border_mode=padsize,
-                )
-                output_scales[cnt_output_response] = filter_scales[idx_filter]
-                cnt_output_response += 1
-
-    return output, output_scales
-
-def calc_n_responses(input_scales, filter_scales):
-
-    return len([ii for ii in input_scales for jj in filter_scales if jj > ii])
-
-
 class FilterBankConvPoolLayer(object):
 
     def __init__(self, input, input_scales, image_shape, filter_bank, filter_scales, filter_shape,
@@ -180,32 +116,32 @@ class FilterBankConvPoolLayer(object):
         if self.lrn:
             self.lrn_func = CrossChannelNormalization()
 
-        n_responses = calc_n_responses(input_scales, filter_scales)
-        self.W = Weight(n_responses)
-        self.b = Weight(n_responses, bias_init, std=0)
-        self.weighted_filter_bank = FilterBankWeight(
-            np.array([self.filter_bank[:, :, :, f_id]*self.W[f_id] for f_id in range(len(self.W))])
-        )
+        # returns a theano.tensor.vector() type variable with indications on the specific response-to-filter
+        # combinations that create frequency decreasing paths:
+        is_apply_filter, _ = theano.scan(fn=lambda Y, X: greater_than_elementwise(X, Y),
+                                         sequences=input_scales,
+                                         non_sequences=filter_scales,
+                                         n_steps=input_scales.shape[0]
+                                         )
+        output_scales = T.tile(filter_scales, input_scales.shape[0])
+        output_scales = output_scales[T.nonzero(is_apply_filter)]
+        # wavenet weights are the factors multiplying each filter-to-featuremap convolution:
+        self.W = Weight((input_scales.shape[0], filter_scales.shape[0]))
+        self.b = Weight(is_apply_filter.shape[0], bias_init, std=0)
 
         input_shuffled = input.dimshuffle(3, 0, 1, 2)  # c01b to bc01
-        # in01out to outin01
-        # print image_shape_shuffled
-        # print filter_shape_shuffled
-        W_shuffled = self.weighted_filter_bank.val.dimshuffle(3, 0, 1, 2)  # c01b to bc01
-        conv_out_complex, output_scales = progress_scatter_layer(input=input_shuffled,
-                                                  input_scales=self.input_scales,
-                                                  filter_bank=W_shuffled,
-                                                  filter_scales=filter_scales,
-                                                  weights=self.W,
-                                                  padsize=padsize,
-                                                  convstride=convstride
-                                                  )
-        conv_out = T.sqrt(T.sqr(T.real(conv_out_complex)) + T.sqr(T.imag(conv_out_complex))) 
+
+        conv_out_complex = theano.scan(fn=lambda I, f, W, cs, ps, flg: progress_single_image(I, f, W, cs, ps, flg),
+                                       sequences=input_shuffled,
+                                       non_sequences=[self.filter_bank, self.W, (convstride, convstride), padsize, is_apply_filter],
+                                       n_steps=input_shuffled.shape[0]
+                                       )
+        conv_out = T.sqrt(T.sqr(T.real(conv_out_complex)) + T.sqr(T.imag(conv_out_complex)))
         conv_out = conv_out + self.b.val.dimshuffle('x', 0, 'x', 'x')
 
         # ReLu
         self.output = T.maximum(conv_out, 0)
-        self.output_scales
+        self.output_scales = output_scales
 
         # Pooling
         if self.poolsize != 1:
@@ -214,7 +150,7 @@ class FilterBankConvPoolLayer(object):
                                        stride=(poolstride, poolstride))
 
         self.output = self.output.dimshuffle(1, 2, 3, 0)  # bc01 to c01b
-
+        assert self.ouput.shape[0] == self.output_scales.shape[0]
         # LRN
         if self.lrn:
             self.output = self.lrn_func(self.output)
